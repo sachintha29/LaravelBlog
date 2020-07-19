09670c0761ebbd28e4a9eb0e42ff1e4d9405a0fd
sachintha29 <sachin133hm@gmail.com>
2020-07-19 09:59:06 +0530

    First commit

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+
+@section('content')
+<a href="/posts" class="btn btn-default">Go Back </a>
+<br>
+<h1>{{$post->title}}</h1>
+<div class="row">
+    <div class="col-md-6">
+        
+    <img style="width: 100%" src="/storage/cover_images/{{$post->cover_image}}" alt="">
+    </div>
+</div>
+<p>{{$post->body}}</p>
+
+<hr>
+
+<small>Wriiten on {{$post->created_at}}</small>
+
+<hr> 
+    @if (!Auth::guest())
+        @if( Auth::user()->id == $post->user_id)
+            <a href="/posts/{{$post->id}}/edit"  class="btn btn-default">Edit</a>
+
+            {!! Form::open(['action' => ['PostsController@destroy',$post->id ],'method' =>'POST', 'calss'=> 'pull-right']) !!}
+            {{ Form::hidden('_method', 'DELETE') }}
+            {{ Form::submit('Delete',['class'=> 'btn btn-danger']) }}
+
+            {!! Form::close() !!}
+        @endif
+    @endif
+
+@endsection
